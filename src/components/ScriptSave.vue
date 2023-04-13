d06d7487127f8e9c3c52afec8ad194c9ee9cf0e1
yangfeng20 <2478341687@qq.com>
2023-04-14 00:16:27 +0800

    feat:文件上传

--- src/components/ScriptSave.vue
+++ src/components/ScriptSave.vue
@@ -3,43 +3,57 @@
     <el-form :model="scriptInfo">
 
       <el-form-item label="脚本名称" :label-width="formLabelWidth">
-        <el-input v-model="scriptInfo.scriptName" autocomplete="off" placeholder="需要定制的脚本简短描述"></el-input>
+        <el-input v-model="scriptInfo.scriptName" autocomplete="off"></el-input>
       </el-form-item>
 
-      <el-form-item label="脚本文件" :label-width="formLabelWidth">
+      <el-form-item label="脚本描述" :label-width="formLabelWidth">
         <el-input v-model="scriptInfo.scriptDesc" autocomplete="off"
-                  placeholder="需要定制脚本的网站url"></el-input>
+        ></el-input>
       </el-form-item>
 
-      <FileUpload @upload="handleUpload" />
+      <el-form-item label="脚本文件"/>
+      <div>
+        <div style="width: 100px"></div>
+        <el-upload drag action="#"
+                   ref="upload"
+                   :file-list="fileList"
+                   :multiple="false"
+                   :auto-upload="false"
 
-      <el-form-item label="账号信息" :label-width="formLabelWidth">
-        <el-input v-model="scriptInfo.accountInfo" autocomplete="off"
-                  placeholder="定制脚本网站的账号（用于站长制造调试脚本，不会在列表显示）"></el-input>
-      </el-form-item>
+                   :on-change="handleChange">
+          <i class="el-icon-upload"></i>
+          <div class="el-upload__text">将脚本拖到此处，或<em>点击上传</em></div>
+        </el-upload>
+        <div v-if="!scriptInfo.fileName"><br></div>
+        <el-tag type="info" v-text="scriptInfo.fileName" v-show="scriptInfo.fileName"></el-tag>
+      </div>
+
 
       <el-form-item label="脚本教程" :label-width="formLabelWidth">
         <quill-editor v-model="scriptInfo.scriptCourse" :options="editorOption"
                       style="height: 500px"></quill-editor>
       </el-form-item>
     </el-form>
+
+    <el-progress type="circle" :percentage="schedule" status="success" ref="progress"></el-progress>
+
+    <el-button type="primary" @click="commitScript">提交脚本</el-button>
   </div>
 </template>
 
 <script>
-import FileUpload from "@/components/FileUpload";
+import ElementUI from 'element-ui'
 
 export default {
-  components: {
-    FileUpload
-  },
+
   name: "ScriptSave",
   data() {
     return {
+      fileList: [],
       scriptInfo: {},
       formLabelWidth: '80px',
       editorOption: {
-        placeholder: '请输入定制脚本的描述\n包含需要定制脚本的网站地址\n网站账号',
+        placeholder: '脚本教程',
         modules: {
           toolbar: [
             ['bold', 'italic', 'underline', 'strike'],
@@ -50,21 +64,60 @@ export default {
           ],
         },
       },
+      commitPercentage: 0,
+      schedule: 0,
     }
   },
+
 
   watch: {
     $route(to, from) {
       this.refresh()
-    }
+    },
   },
   created() {
     this.refresh()
   },
 
   methods: {
-    handleUpload(){
-      console.log("aaaaa")
+    handleChange(file, fileList) {
+      if (fileList.length > 1) {
+        fileList.shift()
+      }
+
+      if (file.status === 'ready') {
+        this.scriptInfo.fileName = file.name
+
+        this.fileToBase64(file.raw).then(dataStr => {
+          this.scriptInfo.byteContentStr = dataStr
+        })
+      }
+    },
+    fileToBase64(file) {
+      this.schedule = 0
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader()
+        // 监听读取进度
+        reader.onprogress = (event) => {
+          if (event.lengthComputable) {
+            // 更新进度
+            this.schedule = Math.round((event.loaded / event.total) * 100)
+          }
+        }
+        reader.onload = () => {
+          const base64 = reader.result
+          resolve(base64)
+        }
+        reader.onerror = reject
+        reader.readAsDataURL(file)
+      })
+    },
+    commitScript() {
+      let url = this.scriptInfo?.id ? "update" : "add";
+      this.$axios.post("/script_info/" + url, this.scriptInfo).then(resp => {
+        this.commitPercentage = 0
+        ElementUI.Message.success("脚本提交成功");
+      })
     },
     refresh() {
       let id = this.$route.query.id;
@@ -82,6 +135,7 @@ export default {
 
 }
 </script>
+
 
 <style scoped>
 
