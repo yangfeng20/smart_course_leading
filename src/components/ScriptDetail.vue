12e411b588dfb1421a97ebe16a3138ecbf905165
yangfeng20 <2478341687@qq.com>
2024-03-09 00:31:23 +0800

    perf:脚本详情页优化

--- src/components/ScriptDetail.vue
+++ src/components/ScriptDetail.vue
@@ -1,71 +1,78 @@
 <template>
   <div>
-    <el-card>
-      <div class="container">
-        <el-page-header @back="windowWith.history.back()" title="返回列表" content="脚本详情">
-        </el-page-header>
-        <div class="box1" slot="header">
-          <h2>{{ scriptInfo.scriptName }}</h2>
-          <el-tag :type="scriptInfo?.scriptStatus?.key === 1 ? 'info' : 'success'">{{
-              scriptInfo?.scriptStatus?.desc
-            }}
-          </el-tag>
-        </div>
 
-        <div>
-          <el-button style="transform: translateY(-50%);" type="primary" round icon="el-icon-download"
-                     @click="downAndUpdateCount">点击下载
-          </el-button>
-        </div>
+    <el-container>
+      <el-header>
+        <el-card>
+          <div class="container">
+            <el-page-header @back="windowWith.history.back()" title="返回列表" content="脚本详情">
+            </el-page-header>
+            <div class="box1" slot="header">
+              <h2>{{ scriptInfo.scriptName }}</h2>
+              <el-tag style="margin-top: 10px;margin-left: 10px;" :type="scriptInfo?.scriptStatus?.key === 1 ? 'info' : 'success'">{{ scriptInfo?.scriptStatus?.desc }}</el-tag>
+            </div>
+            <div>
+              <el-button style="transform: translateY(-50%);" type="primary" round icon="el-icon-download"
+                         @click="downAndUpdateCount">点击下载
+              </el-button>
+            </div>
 
-      </div>
+          </div>
 
-      <el-divider></el-divider>
+          <div style="display: flex; align-items: center">
+            <h3>脚本简介：</h3><span style="font-size: 14px">{{ scriptInfo.scriptDesc }}</span>
+          </div>
+          <div style="display: flex; align-items: center">
+            <label style="font-size: 14px">下载次数：</label>
+            <span style="font-size: 14px">{{ scriptInfo.downloadCount }}</span>
+          </div>
 
-
-      <div style="display: flex; align-items: center">
-        <h3>脚本简介：</h3><span>{{ scriptInfo.scriptDesc }}</span>
-      </div>
-      <div style="display: flex; align-items: center">
-        <label>下载次数：</label>
-        <span>{{ scriptInfo.downloadCount }}</span>
-      </div>
-
-      <el-divider></el-divider>
-
-      <div style=" align-items: center">
-        <h3>脚本教程：</h3>
-        <RichTextShowReadonly :content="scriptInfo?.scriptCourse"/>
-      </div>
+        </el-card>
 
 
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <div style="display: flex; align-items: center">
-        <label>脚本创建时间：</label>
-        <span>{{ scriptInfo.createdDate }}</span>
-      </div>
+      </el-header>
+      <el-container style="padding-top: 100px;">
 
-      <div style="display: flex; align-items: center" v-show="scriptInfo.updatedDate">
-        <label>脚本更新时间：</label>
-        <span>{{ scriptInfo.updatedDate }}</span>
-      </div>
-    </el-card>
+        <el-main>
+          <el-card>
+            <div style=" align-items: center">
+              <h3>脚本教程：</h3>
+              <RichTextShowReadonly style="transform: translateY(0%);" :content="scriptInfo?.scriptCourse"/>
+            </div>
+          </el-card>
+        </el-main>
+
+        <el-aside width="400px" style="margin-top: 20px;margin-right: 20px">
+          <div>
+            <el-card class="card" shadow="hover">
+              <div style="display: flex; align-items: center">
+                <label>脚本创建时间：</label>
+                <span>{{ scriptInfo.createdDate }}</span>
+              </div>
+
+              <div style="display: flex; align-items: center" v-show="scriptInfo.updatedDate">
+                <label>脚本更新时间：</label>
+                <span>{{ scriptInfo.updatedDate }}</span>
+              </div>
+            </el-card>
+          </div>
+          <Sponsor></Sponsor>
+        </el-aside>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
 import RichTextShowReadonly from "../components/RichTextShowReadonly";
 import ElementUI from "element-ui";
-import qs from "qs";
+import Sponsor from "../components/Sponsor";
 
 export default {
   name: "ScriptDetail",
   components: {
     RichTextShowReadonly,
+    Sponsor,
   },
   data() {
     return {
@@ -122,13 +129,18 @@ export default {
 
 .box1 {
   width: 50%;
-  height: 100px;
+  height: 40px;
   float: left;
   margin-right: 1px;
+  display: flex;
 }
 
 .box2 {
   width: 50%;
   height: 100px;
+}
+
+.card {
+margin-bottom: 10px;
 }
 </style>
